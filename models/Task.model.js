bb36c57dc4f39252eea1076e8d62aed73174ab30
hleonn <edhleon2001@hotmail.com>
2022-07-12 22:03:24 -0500

    models rooms and tasks

--- models/Task.model.js
+++ models/Task.model.js
@@ -1,20 +1,20 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const rooms = new Schema({
-    name:{
+const taskSchema = new Schema({
+    title:{
         type: String,
         trim: true,
         minLength: 2, 
         required: true,
     },
-    typeHab:{
+    
+    status:{
         type: String,
-    },
-    ub:{
-        type: String
-    }, 
-    description:{
-        type: String,
+        required: true,
     }
-})
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
